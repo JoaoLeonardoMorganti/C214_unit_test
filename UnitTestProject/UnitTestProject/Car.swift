0e0ee93d14700db6cc2e12e6217c72c4c0dd3f5d
João Leonardo Morganti <joaosilva.e-deploy@natura.net>
2021-05-05 14:38:02 -0300

    Create Car class

---
+++ UnitTestProject/UnitTestProject/Car.swift
@@ -0,0 +1,50 @@
+//
+//  Car.swift
+//  UnitTestProject
+//
+//  Created by João Leonardo Morganti on 05/05/21.
+//
+
+import Foundation
+
+class Car {
+    var miles = 0
+    var type: CarType
+    var transmissionMode: CarTransmissionMode
+    
+    init(type:CarType, transmissionMode:CarTransmissionMode){
+        self.type = type
+        self.transmissionMode = transmissionMode
+    }
+    
+    func start(minutes: Int) {
+        var speed = 0
+        
+        if self.type == .Economy && self.transmissionMode == .Drive {
+            speed = 35
+        }
+        
+        if self.type == .OffRoad && self.transmissionMode == .Drive {
+            speed = 50
+        }
+        
+        if self.type == .Sport && self.transmissionMode == .Drive {
+            speed = 70
+        }
+        
+        self.miles = speed * (minutes / 60)
+    }
+}
+
+enum CarType {
+    case Economy
+    case OffRoad
+    case Sport
+}
+
+enum CarTransmissionMode {
+    case Park
+    case Reverse
+    case Neutral
+    case Drive
+}
